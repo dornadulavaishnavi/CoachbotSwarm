536ad71c770b050affc4b481ec7c07312b648ae4
vdornadula <vaishnavidornadula2026@u.northwestern.edu>
2023-08-09 10:32:35 -0500

    added an example template for required files

---
+++ Example_Folder/usr_code.py
@@ -0,0 +1,9 @@
+import math
+import struct
+
+def usr(robot):
+    # any set up variables or code before looping can go here
+    while True:
+        robot.delay()
+        # your looping code here
+        return

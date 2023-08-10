c6c4d11f02fa80ebe99bcc29bee314ca4deb5d82
vdornadula <vaishnavidornadula2026@u.northwestern.edu>
2023-08-10 08:50:48 -0500

    added logging to example script

--- Example_Folder/usr_code.py
+++ Example_Folder/usr_code.py
@@ -3,7 +3,14 @@ import struct
 
 def usr(robot):
     # any set up variables or code before looping can go here
+    log = open("experiment_log", "wb")
+    
+    log.write("an example write string")
+    log.flush()
+
     while True:
         robot.delay()
         # your looping code here
+
+        log.close()
         return

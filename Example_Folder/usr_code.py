d356372d0b9881ca4853fe1b1933b1942fda24a2
vdornadula <vaishnavidornadula2026@u.northwestern.edu>
2023-08-16 10:33:25 -0500

    added delay to example folder

--- Example_Folder/usr_code.py
+++ Example_Folder/usr_code.py
@@ -2,6 +2,7 @@ import math
 import struct
 
 def usr(robot):
+    robot.delay(3000) # ensures that the camera and all other peripherals are up and running before your code begins
     # any set up variables or code before looping can go here
     log = open("experiment_log", "wb")
     

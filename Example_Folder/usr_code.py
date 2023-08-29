8f1ec3cca14d19952e057c61a8439bbd6c18850d
vdornadula <vaishnavidornadula2026@u.northwestern.edu>
2023-08-29 09:04:16 -0500

    added new line to example code string

--- Example_Folder/usr_code.py
+++ Example_Folder/usr_code.py
@@ -6,7 +6,7 @@ def usr(robot):
     # any set up variables or code before looping can go here
     log = open("experiment_log", "wb")
     
-    log.write("an example write string")
+    log.write("an example write string\n")
     log.flush()
 
     while True:

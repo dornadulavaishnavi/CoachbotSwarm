752bb33f1c9de26b072d6a52029dd984304ba4af
hanlinwang2015 <hanlinwang1025@u.northwestern.edu>
2023-09-16 10:46:26 -0500

    from script

---
+++ Completed_Runs/Miketest_recording_vaish/usr_code.py
@@ -0,0 +1,25 @@
+import math
+import struct
+
+def usr(robot):
+	robot.delay(3000)
+	while True:
+	# your looping code here
+		robot.set_led(0,100,0)
+		robot.set_vel(-10,10)
+		robot.delay(500)
+		for j in range(10):
+			for i in range(5):
+				robot.set_led(100,0,0)
+				for i in range(5):
+					robot.set_led(90,90,100)
+					robot.set_vel(10,10)
+					robot.delay(100)
+				for i in range(5):
+					robot.set_led(0,0,0)
+					robot.set_vel(-10,-10)
+					robot.delay(100)
+			robot.set_led(0,0,100)
+			robot.set_vel(-20,20)
+			robot.delay(400)
+		return

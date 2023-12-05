f003d36a5960ba614e6ee49d06e1bfd2eb3509fe
hanlinwang2015 <hanlinwang1025@u.northwestern.edu>
2023-12-04 18:11:05 -0600

    from script

--- Completed_Runs/Muhan_test/usr_code.py
+++ Completed_Runs/Muhan_test/usr_code.py
@@ -1,214 +1,145 @@
-import time
 import math
 import struct
-
-
-def get_theta(unit_vector):
-    x, y = unit_vector.x, unit_vector.y
-    if y >= 0:
-        return math.acos(x)
-    else:
-        return -math.acos(x)
-
-
-def get_theta_diff(theta_target, theta_current):
-    diff = theta_target - theta_current
-    if math.fabs(diff) > math.pi:
-        if theta_target > theta_current:
-            # target > 0, current < 0
-            return math.pi * 2 - theta_target + theta_current
-        else:
-            # target < 0, current > 0
-            return theta_current - math.pi * 2 - theta_target
-    else:
-        return -diff
-
-
-def get_pose_safe(robot):
-    pose = None
-    while not pose:
-        pose = robot.get_pose()
-    return Vector2D(pose[0], pose[1]), pose[2]
-
-
-def clip(value, min, max):
-    if value < min:
-        return min
-    elif value > max:
-        return max
-    return value
-
-
-class Vector2D:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def length(self):
-        return math.sqrt(self.x**2 + self.y**2)
-
-    def __sub__(self, other):
-        if isinstance(other, Vector2D):
-            return Vector2D(self.x - other.x, self.y - other.y)
-        elif isinstance(other, (int, float)):
-            return Vector2D(self.x - other, self.y - other)
-        else:
-            raise TypeError("Unsupported operand type for subtraction")
-
-    def __add__(self, other):
-        if isinstance(other, Vector2D):
-            return Vector2D(self.x + other.x, self.y + other.y)
-        elif isinstance(other, (int, float)):
-            return Vector2D(self.x + other, self.y + other)
-        else:
-            raise TypeError("Unsupported operand type for addition")
-
-    def __truediv__(self, value):
-        return Vector2D(self.x / value, self.y / value)
-
-    def __mul__(self, value):
-        return Vector2D(self.x * value, self.y * value)
-
-    def __repr__(self):
-        return "(" + str(self.x) + ", " + str(self.y) + ")"
-
-
-class PositionEstimate:
-    def __init__(self, name=""):
-        self.last_position = None
-        self.last_time = None
-        self.speed_estimate = None
-        self.name = name
-
-    def update(self, new_position):
-        new_time = time.time()
-        if self.last_position is not None:
-            delta_time = new_time - self.last_time
-            self.speed_estimate = (new_position - self.last_position) / delta_time
-        self.last_position = new_position
-        self.last_time = new_time
-
-    def get_position_estimate(self):
-        if self.speed_estimate is not None:
-            passed_time = time.time() - self.last_time
-            return self.speed_estimate * passed_time + self.last_position
-        else:
-            return None
-
-    def get_speed_estimate(self):
-        return self.speed_estimate
-
+import numpy as np
 
 def usr(robot):
-    desired_distance = 0.3  # will vary from 0.3-0.5
-
-    ################################
-    # CONFIGURATIONS
-    ################################
-    robot_1_orbit_speed = 10
-    robot_1_correction_max_speed = 10
-    # maximum bound of absolute distance error when we apply the max correction speed
-    # If a more stable trace is desired, use 0.1 (looser bound),
-    # otherwise use 0.05 (tighter bound) to keep robot within desired distance
-    robot_1_correction_distance_bound = 0.1
-    # weight for mapping speed estimate to wheel speed
-    robot_1_speed_estimate_weight = 100
-    # weight for mapping difference between target speed direction and current direction
-    # to wheel speed
-    robot_1_wheel_diff_weight = 5
-
-    ################################
-    # STATES
-    ################################
-    robot.delay(3000)
+    robot.delay(3000) # ensures that the camera and all other peripherals are up and running before your code begins
+    # any set up variables or code before looping can go here
     log = open("experiment_log", "wb")
-    robot_0_position_estimate = PositionEstimate("robo0")
-    change_timer = time.time()
-    robot_id = robot.assigned_id
-    log.write("start")
+    
+    log.write("an example write string\n")
+    log.flush()
+    
+    desired_distance = 0.3 # will vary from 0.3-0.5
+    
+    # Init storage
+    r0x = 0.0
+    r0y = 0.0
+    x0 = 0.0
+    y0 = 0.0
+    
+    # Init err storage
+    total_err = 0
+    d = None
+
     while True:
-        # robot.delay()
-        if robot_id == 0:
-            position, direction = get_pose_safe(robot)
-            robot.send_msg(struct.pack("ff", position.x, position.y))
-
+        robot.delay()
+        if (robot.virtual_id() == 0):
+            log.write("Is robot 0\n")
+            log.flush()
+            # if we received a message, print out info in message
             msgs = robot.recv_msg()
-            log.write("robot 0 received message")
             if len(msgs) > 0:
-                robot_1_position = Vector2D(*struct.unpack("ff", msgs[0][:8]))
-                distance = (position - robot_1_position).length()
+                pose_rxed = struct.unpack('ffi', msgs[0][:12])
+                #print('robot ', robot.id, ' received position ', pose_rxed[0], pose_rxed[1], ' from robot ', pose_rxed[2])
 
-                if distance > desired_distance:
-                    # red
-                    robot.set_led(100, 0, 0)
-                else:
-                    # green
-                    robot.set_led(0, 100, 0)
+                # Grab robot 1 coordinates
+                x1 = pose_rxed[0]
+                y1 = pose_rxed[1]
 
-        if robot_id == 1:
+                # Calculate distance, print, and color robot 
+                robot_dist = np.sqrt((x0-x1)**2+(y0-y1)**2)
+                # print("The distance between the two robots is: "+str(robot_dist))
+                if(robot_dist > desired_distance): robot.set_led(100,0,0)
+                else: robot.set_led(0,100,0)
+
+            pose_t = robot.get_pose()
+            # if there is a new postion sensor update, print out and transmit the info
+            if pose_t:  # check pose is valid before using
+                #print('The x,y postion of robot ', robot.id, ' is ', pose_t[0], pose_t[1])
+                robot.send_msg(struct.pack('ffi', pose_t[0], pose_t[1], robot.id))  # send pose x,y in message
+                # Grab robot 0 coordinates
+                x0 = pose_t[0]
+                y0 = pose_t[1]    
+
+            robot.set_vel(0, 0)
+
+        if (robot.virtual_id() == 1):
+            log.write("Is robot 1\n")
+            log.flush()
+            # if we received a message, print out info in message
             msgs = robot.recv_msg()
-            log.write("robot 1 received message")
             if len(msgs) > 0:
-                robot_0_position = Vector2D(*struct.unpack("ff", msgs[0][:8]))
-                # Update position estimate of robot 0 when receiving position information
-                robot_0_position_estimate.update(robot_0_position)
+                pose_rxed = struct.unpack('ffi', msgs[0][:12])
+                #print('robot ', robot.id, ' received position ', pose_rxed[0], pose_rxed[1], ' from robot ', pose_rxed[2])
 
-            position, direction = get_pose_safe(robot)
+                # Grab r0 positions
+                # If some more advanced control was required, would want to 
+                # keep a buffer to estimate velocity. Uneeded for now.
+                r0x = pose_rxed[0]
+                r0y = pose_rxed[1]
 
-            # send current position information to robot 0
-            robot.send_msg(struct.pack("ff", position.x, position.y))
+            pose_t = robot.get_pose()
+            # if there is a new postion sensor update, print out and transmit the info
+            if pose_t:  # check pose is valid before using
+                #print('The x,y postion of robot ', robot.id, ' is ', pose_t[0], pose_t[1])
+                robot.send_msg(struct.pack('ffi', pose_t[0], pose_t[1], robot.id))  # send pose x,y in message
 
-            # update control every time we get current position
-            # we cannot use position estimate on robot1 because the update
-            # speed will be much higher, and in every update the speed of
-            # robot 1 is updated, resulted in a high error
-            robot_0_position = robot_0_position_estimate.get_position_estimate()
-            if robot_0_position is not None:
-                # position difference vector from robot 1 to 0
-                position_diff_vec = robot_0_position - position
+                # Acquire Pose
+                r1x = pose_t[0]
+                r1y = pose_t[1]
+            
+            # Calculate distance in x, y
+            del_x = r0x-r1x
+            del_y = r0y-r1y
+            
+            # Determine distance between two robots 
+            # Include some logic for starting error measurement
+            if(d is None): 
+                d = np.sqrt((del_x)**2 + (del_y)**2)
+                last_d = d
+            else:
+                last_d = d
+                d = np.sqrt((del_x)**2 + (del_y)**2)
+                
+            # Set robot to proper color 
+            # Green = too close or exact
+            # Red = too far
+            if(d > desired_distance): robot.set_led(100,0,0)
+            else: robot.set_led(0,100,0)
 
-                # unit vector of orbit speed term, always vertical to
-                # vector pointing from robot 1 to 0
-                orbit_speed_u_vec = Vector2D(-position_diff_vec.y, position_diff_vec.x)
-                orbit_speed_u_vec = orbit_speed_u_vec / orbit_speed_u_vec.length()
+            # Calculate error statistics 
+            err = d - desired_distance
+            diff_err = d-last_d
+            total_err += err
+            total_err = min(max(total_err,-100),100) # Arbitrary limits
 
-                # unit vector of correction speed term, used for correcting
-                # distance error between robot 1 and 0
-                correction_speed_u_vec = position_diff_vec / position_diff_vec.length()
+            # kP needs to have sufficent power to correct and to follow a 
+            # moving target
+            kP = 45
+            
+            # Main purpose of kD is to damp the oscillation that comes with 
+            # Turning in and out of the zone 
+            kD = 100
+            
+            # Determined that kI was not needed and also adding kI can be scary
+            # for longer term sims or when the robot_0 is moving 
+            # Left in control in case of tuning later. 
+            kI = 0.0
+            
+          
+            # The wheels always move forward but their relative speeds are 
+            # affected by the measured error. There is a slight differnece 
+            # in their 0 error speeds to force a CW rotaiton
+            
+            # If the robot is too far (err > 0), speed up the outside wheel 
+            # and slow inside wheel to turn in a closer circle. 
+            
+            # If robot is too close (err < 0), slow the outside wheel and 
+            # speed up the inside wheel to turn in a wide circle
+            
+            # This will only for for clockwise orbiting.
+            whl_1_spd = 11.125 + kP * (err) + kD * (diff_err) + kI*total_err
+            whl_2_spd = 8.875 - kP * (err) - kD * (diff_err) - kI*total_err
+                
+            #Set wheel velocities left=outside, right=inside
+            robot.set_vel(whl_1_spd, whl_2_spd)
 
-                # positive correction_strength is attractive and negative is repulsive
-                correction_strength = clip(
-                    (position_diff_vec.length() - desired_distance)
-                    / robot_1_correction_distance_bound,
-                    -1,
-                    1,
-                )
+            
+            
+            
+        # your looping code here
 
-                # final speed vector is a mixture of orbit speed, correction speed,
-                # and adding the speed estimate of robot 0
-                speed_vec = (
-                    orbit_speed_u_vec * robot_1_orbit_speed
-                    + correction_speed_u_vec
-                    * correction_strength
-                    * robot_1_correction_max_speed
-                    + robot_0_position_estimate.get_speed_estimate()
-                    * robot_1_speed_estimate_weight
-                )
-
-                # move wheels
-                speed = speed_vec.length()
-                speed_direction = get_theta(speed_vec / speed)
-
-                # compute minimum theta difference between target speed direction
-                # and current direction
-                min_diff = get_theta_diff(speed_direction, direction)
-
-                # correct speed using a proportional controller
-                robot_1_wheel_speeds = [
-                    speed + robot_1_wheel_diff_weight * min_diff,
-                    speed - robot_1_wheel_diff_weight * min_diff,
-                ]
-
-                robot.set_vel(robot_1_wheel_speeds[0], robot_1_wheel_speeds[1])
     log.close()
     return
+
